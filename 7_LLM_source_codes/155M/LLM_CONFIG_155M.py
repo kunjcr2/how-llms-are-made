186ca6d51cc9e9864802717271dfb9fb5c50c84c
kunjcr2 <kunjshah1506@hotmail.com>
2025-05-28 18:09:25 -0700

    Files added for both LLMs

---
+++ 7_LLM_source_codes/155M/LLM_CONFIG_155M.py
@@ -0,0 +1,9 @@
+GPT_CONFIG_124M = {
+    "vocab_size": 50257,  # Vocabulary size
+    "context_length": 256,  # Context length
+    "emb_dim": 768,  # Embedding dimension
+    "n_layers": 12,  # Number of
+    "n_heads": 12,  # Number of attention heads per transformer block
+    "drop_rate": 0.1,  # Dropout rate
+    "qkv_bias": False,  # Query-Key-Value bias
+}
---
+++ 7_LLM_source_codes/211M/LLM_CONFIG_211M.py
@@ -0,0 +1,9 @@
+GPT_CONFIG_124M = {
+    "vocab_size": 50257,  # Vocabulary size
+    "context_length": 256,  # Context length
+    "emb_dim": 810,  # Embedding dimension
+    "n_layers": 18,  # Number of
+    "n_heads": 18,  # Number of attention heads per transformer block
+    "drop_rate": 0.2,  # Dropout rate
+    "qkv_bias": False,  # Query-Key-Value bias
+}
