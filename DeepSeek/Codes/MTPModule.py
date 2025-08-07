a10859dd6c497d5fff0c02d8a7e01e33606dbca4
kunjcr2 <kunjshah1506@hotmail.com>
2025-08-07 07:45:26 -0700

    MTP Structure created

---
+++ DeepSeek/Codes/MTPModule.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class MTPModule(nn.Module):
+
+    def __init__(self):
+        """
+        Initializing metrics for multi token predicition
+        """
+        pass
+
+    def forward(self, x):
+        """
+        Passing hidden state x to mtp module
+        """
